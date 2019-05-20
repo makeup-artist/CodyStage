f7875868cab856574d21dfcb441c1709ad7b22f8
zhangqi2 <zhangqi2@sensetime.com>
2019-05-20 20:43:14 +0800

    .

---
+++ src/main/resources/sql/Goods.sql
@@ -0,0 +1,15 @@
+create table if not exists `goods`
+(
+    id          int unsigned auto_increment             not null comment '商品id'
+        primary key,
+    name        varchar(55)                             not null comment '商品名称',
+    picture     varchar(100)                            not null comment '商品首页图片url',
+    description varchar(300)                            not null comment '商品概要描述',
+    details     varchar(4096) default ''                null comment '商品详细描述',
+    type        tinyint unsigned                        not null comment '1口红 2底妆 3眉毛 4美瞳 5发色',
+    price       double                                  not null comment '商品价格',
+    createTime  timestamp     default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime  timestamp     default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更改时间'
+
+)
+    charset = utf8mb4;
---
+++ src/main/resources/sql/picture.sql
@@ -0,0 +1,12 @@
+create table if not exists `picture`
+(
+    id         int unsigned auto_increment         not null comment '商品图片id'
+        primary key,
+    name       varchar(50)                         not null comment '颜色名称',
+    goods_id   int unsigned                        not null comment '商品id',
+    color      varchar(40)                         null comment '颜色',
+    url        varchar(100)                        not null comment '图片url',
+    createTime timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更改时间'
+)
+    charset = utf8mb4;
